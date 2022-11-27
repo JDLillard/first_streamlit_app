ce49e5d5e16c385dfbfc0ceb1a090c126849622c
JDLillard <119258838+JDLillard@users.noreply.github.com>
2022-11-27 17:46:43 -0500

    Update streamlit_app.py
    Added pandas and drop down select

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,20 @@
 import streamlit
 
-streamlit.title( 'My Parents New Healthy Diner' )
+streamlit.title( 'My Mom's New Healthy Diner' )
 
 streamlit.header( 'Breakfast Favorites' )
 streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal' )
 streamlit.text( '🥗 Kale, Spinich & Rocket Smoothie' )
 streamlit.text( '🐔 Hard-boiled Free-range Egg')
 streamlit.text( '🥑🍞 Avacado Toast' )
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
+
+# Display the table on the page.
